84a40cf88b629319382763c28df19c19449c724e
styled <smyvensestime@gmail.com>
2023-04-18 13:40:05 -0400

    default styles

--- next-app/src/app/layout.tsx
+++ next-app/src/app/layout.tsx
@@ -1,13 +1,20 @@
 import "./globals.tw.css"
+import { Poppins } from "next/font/google"
 
 export const metadata = {
     title: "University management system",
 }
 
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-poppins",
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className={`${poppins.className}`}>{children}</body>
         </html>
     )
 }
