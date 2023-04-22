a3246254fda213f717f0a7bcb9ffe937e6d97cb8
styled <smyvensestime@gmail.com>
2023-04-22 17:55:05 -0400

    responsive home page

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import { FaSchool } from "react-icons/fa"
 export default async function Home() {
     return (
         <main className="flex mx-10 justify-between">
-            <div className="mt-[35vh] text-4xl font-semibold">
+            <div className="mt-[35vh] text-[3vw] font-semibold">
                 <h1>University Management System</h1>
             </div>
             <div className="text-slate-400 text-[30vw] mt-[10vh] mr-[5vw]">
