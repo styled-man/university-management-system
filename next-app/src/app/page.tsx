bdb02355bca641b5fbd004de88048ece5db78dfc
styled <smyvensestime@gmail.com>
2023-04-22 14:51:24 -0400

    made home page

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
+import { FaSchool } from "react-icons/fa"
+
+export default async function Home() {
     return (
-        <main>
-            <div>
-                <h1>Hello World</h1>
+        <main className="flex mx-10 justify-between">
+            <div className="mt-[35vh] text-4xl font-semibold">
+                <h1>University Management System</h1>
+            </div>
+            <div className="text-slate-400 text-[30vw] mt-[10vh] mr-[5vw]">
+                <FaSchool />
             </div>
         </main>
     )
