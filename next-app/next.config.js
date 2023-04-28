50354ce5a10417fa60bd041b79ca4ad3245f90cb
styled-man <smyvensestime@gmail.com>
2023-04-28 07:24:09 -0400

    fixed hot module reload dev | windows

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
+    experimental: {
+        appDir: true,
+    },
 }
 
 module.exports = nextConfig
